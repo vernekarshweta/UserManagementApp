8f6fb0e65b5c9b162f297891c30b43bd197c6b4e
shweta <vernekarsweta@gmail.com>
2026-04-27 22:20:58 +0530

    init code

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+// BUG 1: Missing middleware
+// app.use(express.json())
+
+// BUG 2: CORS not enabled properly
+// app.use(cors)
+
+mongoose.connect('mongodb://127.0.0.1:27017/testdb')
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String
+})
+
+const User = mongoose.model('User', userSchema)
+
+// GET USERS
+app.get('/users', async (req, res) => {
+    const users = await User.find
+    res.json(users)
+})
+
+// ADD USER
+app.post('/users', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email
+    })
+
+    await user.save
+    res.send("User added")
+})
+
+// GET SINGLE USER
+app.get('/users/:id', async (req, res) => {
+    const user = await User.findOne("id": req.params.id)
+    res.json(user)
+})
+
+// DELETE USER
+app.delete('/users/:id', async (req, res) => {
+    await User.deleteOne({ _id: req.params.id })
+    res.send("Deleted")
+})
+
+// BUG 3: No callback
+app.listen(3000)
